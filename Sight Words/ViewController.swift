b7876aebf288abcce8c45d836b41c03e303475fb
Matt Young <youngmatt519@gmail.com>
2024-05-02 22:19:13 -0400

    random initial commit

--- Sight Words/ViewController.swift
+++ Sight Words/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var wordList: [String] = []
+    var currWord: String = ""
+    var tokenCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        wordList = ["good", "elephant", "take", "six", "what"]
+        currWord = wordList.randomElement()!
+        print(currWord)
+
         // Do any additional setup after loading the view.
     }
+    
+    
+    
 
+    @IBOutlet weak var checkBox1: UIImageView!
+    @IBOutlet weak var checkBox2: UIImageView!
+    @IBOutlet weak var checkBox3: UIImageView!
+    @IBOutlet weak var checkBox4: UIImageView!
+    @IBOutlet weak var checkBox5: UIImageView!
 
+    @IBAction func wordButton(_ sender: UIButton) {
+        print(sender.titleLabel?.text!)
+        if sender.titleLabel?.text == self.currWord {
+            print("That is correct!")
+            tokenCount += 1
+            switch tokenCount {
+            case 1:
+                checkBox1.image = UIImage(named: "pinkCheckedBox")
+            case 2:
+                checkBox2.image = UIImage(named: "pinkCheckedBox")
+            default:
+                print("hooray!")
+            }
+        }
+    }
 }
 
